8076c55dd03bc355095d73c5be1a3bd5111669e8
dido1043 <didodido15>
2022-09-08 12:36:25 +0300

    update

---
+++ Stacks and Queues/Stacks and Queues/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stacks_and_Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
