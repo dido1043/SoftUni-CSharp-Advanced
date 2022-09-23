af5ee6974c2605faf412df38984811dfebe8411e
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2022-09-23 16:54:49 +0300

    Update

---
+++ T07.ParkingLot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T07.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                var tokens = command.Split(", ");
+                if (tokens[0] == "IN")
+                {
+                    set.Add(tokens[1]);
+                }
+                else if (tokens[0] == "OUT")
+                {
+                    set.Remove(tokens[1]);
+                }
+                command = Console.ReadLine();
+            }
+            if (set.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+
+                return;
+            }
+
+            List<string> list = set.ToList();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ t2.SetsOfElements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace t2.SetsOfElements
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            HashSet<string> set1 = new HashSet<string>();
+            HashSet<string> set2 = new HashSet<string>();
+            int totalSize = sizes[0] + sizes[1];
+
+            for (int i = 0; i < sizes[0]; i++)
+            {
+                string token = Console.ReadLine();
+                set1.Add(token);
+            }
+
+            for (int i = 0; i < sizes[1]; i++)
+            {
+                set2.Add(Console.ReadLine());
+            }
+            set1.IntersectWith(set2);
+            Console.WriteLine(String.Join(" ", set1));
+        }
+    }
+}
