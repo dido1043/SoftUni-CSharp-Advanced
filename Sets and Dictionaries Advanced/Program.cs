858a55196149e153f81c66cac6491bbeaebedb1b
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2022-09-23 16:47:21 +0300

    ....

---
+++ Sets and Dictionaries Advanced/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T01.CountSameValuesInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+            double[] arr = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+            Dictionary<double, int> dict = new Dictionary<double, int>();
+            foreach (var item in arr)
+            {
+                if (!dict.ContainsKey(item))
+                {
+                    dict.Add(item, 0);
+                }
+                dict[item]++;
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
---
+++ t7.KnightGame/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace _07.KnightGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            if (size < 3)
+            {
+                Console.WriteLine(0);
+
+                return;
+            }
+
+            char[,] matrix = new char[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                string chars = Console.ReadLine();
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = chars[col];
+                }
+            }
+            int knightsRemoved = 0;
+            while (true)
+            {
+                int countMostAttacked = 0;
+                int rowMostAttacked = 0;
+                int colMostaAttacked = 0;
+
+                for (int row = 0; row < size; row++)
+                {
+                    for (int col = 0; col < size; col++)
+                    {
+                        if (matrix[row, col] == 'K')
+                        {
+                            int attacks = CountAttackedKnights(row,col,size,matrix);
+                            if (countMostAttacked < attacks) 
+                            {
+                                countMostAttacked = attacks;
+                                rowMostAttacked = row;
+                                colMostaAttacked = col;
+                            }
+                        }
+                    }
+                }
+
+                if (countMostAttacked == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    matrix[rowMostAttacked, colMostaAttacked] = '0';
+                    knightsRemoved++;
+                }
+
+            }
+            Console.WriteLine(knightsRemoved);
+
+        }
+
+        static int CountAttackedKnights(int row, int col, int size, char[,] matrix)
+        {
+            int attackedKnights = 0;
+
+            //horizontal left-up
+            if (IsCellValid(row-1,col-2,size))
+            {
+                if (matrix[row - 1, col - 2] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            //horizontal left-down
+
+            if (IsCellValid(row + 1, col - 2, size))
+            {
+                if (matrix[row+1, col-2] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+            //horizontal right-up
+            if (IsCellValid(row - 1, col + 2, size))
+            {
+                if (matrix[row-1, col+2] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            //horizontal right-up
+
+            if (IsCellValid(row + 1, col + 2, size))
+            {
+                if (matrix[row + 1,col+2] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+            //vertical up-left
+            if (IsCellValid(row-2,col-1,size))
+            {
+                if (matrix[row - 2, col - 1] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            //vertical up-right
+
+            if (IsCellValid(row - 2, col + 1, size))
+            {
+                if (matrix[row - 2, col + 1] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+            //vertical down-left
+
+            if (IsCellValid(row + 2, col - 1, size))
+            {
+                if (matrix[row + 2, col - 1] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+            //vertical down-right
+
+            if (IsCellValid(row + 2, col + 1, size))
+            {
+                if (matrix[row + 2, col + 1] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+            return attackedKnights;
+        }
+
+        static bool IsCellValid(int row, int col, int size)
+        {
+            return row >= 0 && row < size && col >= 0 && col < size;
+        }
+    }
+}
