d056657a06141d84797cf8e4a6399a7a0b0e8696
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2022-09-27 18:59:21 +0300

    ....

---
+++ EXAM_PREP/Drones/Drones/Airfield.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Drones
+{
+    public class Airfield
+    {
+        
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public double LandingStrip { get; set; }
+        public List<Drone> Drones { get; private set; }
+
+
+        public Airfield( string name, int capacity, double landingStrip)
+        {
+
+            this.Name = name;
+            this.Capacity = capacity;
+            this.LandingStrip = landingStrip;
+            this.Drones = new List<Drone>();
+        }
+        //Solution
+        //-----------------------------------------
+        //Count
+        public int Count => this.Drones.Count;
+
+        //Adding drone
+        public string AddDrone(Drone drone)
+        {
+            if (string.IsNullOrEmpty(drone.Name) ||
+                string.IsNullOrEmpty(drone.Brand) ||
+                drone.Range < 5 || drone.Range > 15)
+            {
+                return "Invalid drone.";
+            }
+
+            if (this.Capacity <= this.Count)
+            {
+                return "Airfield is full.";
+            }
+
+            this.Drones.Add(drone);
+            return $"Successfully added {drone.Name} to the airfield.";
+        }
+        //Remove drone
+
+        public bool RemoveDrone(string name)
+        {
+            this.Drones.RemoveAll(n => n.Name == name);
+            return Count < 0;
+        }
+        //Remove by brand
+
+        public int RemoveDroneByBrand (string brand)
+        {
+            int count = this.Drones.RemoveAll(x => x.Brand == brand);
+
+            return count;
+        }
+        //Fly drone
+        public Drone FlyDrone(string name)
+        {
+            Drone d = this.Drones.FirstOrDefault(n => n.Name == name);
+            if (d == null)
+            {
+                return null;
+            }
+            d.Available = false;
+           return d;
+        }
+        //List<Drone> FlyDronesByRange(int range)
+
+        public List<Drone> FlyDronesByRange(int range)
+        {            
+           List<Drone> drones = this.Drones.Where(x => x.Range >= range).ToList();
+            foreach (var item in drones)
+            {
+                item.Available = false;
+            }
+            return drones;
+
+        }
+        //	Report
+        public string Report()
+        {
+            var available = this.Drones.Where (x => x.Available == true);
+            return $"Drones available at {this.Name}" +
+                $"{string.Join("\n",available)}";
+
+        }
+
+    }
+}
---
+++ EXAM_PREP/Problems/Problem1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meals = Console.ReadLine().Split(" ");
+            int[] calories = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Queue<string> mealsQueue = new Queue<string>(meals);
+            Stack<int> caloriesStack = new Stack<int>(calories);
+            //Solution
+
+            int currentCalories = 0;
+            string currentMeal = "";
+            int edited = 0;
+            int counter = 0;
+            int diff = 0;
+
+            bool isHaveFood = true;
+            while (mealsQueue.Count >= 0 && caloriesStack.Count > 0)
+            {
+                currentCalories = caloriesStack.Pop() - Math.Abs(diff);
+                if (mealsQueue.Count == 0)
+                {
+                    caloriesStack.Push(currentCalories);
+                    break;
+                }
+                while (currentCalories > 0)
+                {
+                    currentMeal = mealsQueue.Peek();
+                    if (currentMeal == "salad")
+                    {
+                        currentCalories -= 350;
+                        mealsQueue.Dequeue();
+                    }
+                    else if (currentMeal == "soup")
+                    {
+                        currentCalories -= 490;
+                        mealsQueue.Dequeue();
+                    }
+                    else if (currentMeal == "pasta")
+                    {
+                        currentCalories -= 680;
+                        mealsQueue.Dequeue();
+                    }
+                    else if (currentMeal == "steak")
+                    {
+                        currentCalories -= 790;
+                        mealsQueue.Dequeue();
+                    }
+                    
+                    counter++;
+                }
+                diff = currentCalories;
+                currentCalories = 0;
+            }
+            if (mealsQueue.Count == 0)
+            {
+                Console.WriteLine($"John had {counter} meals. \n" +
+                $"For the next few days, he can eat {string.Join(", ", caloriesStack)} calories.");
+                return;
+            }
+
+            if (caloriesStack.Count == 0)
+            {
+                Console.WriteLine($"John ate enough, he had {counter} meals. \n" +
+                                  $"Meals left: {string.Join(", ", mealsQueue)}.");
+
+                return;
+            }
+        }
+    }
+}
