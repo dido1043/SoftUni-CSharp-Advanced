5b84d490a83cb162ac93e46ddd8bcea76f342b42
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2022-10-08 22:44:18 +0300

    problems

---
+++ EXAM_PREP/Renovators/Renovators/StartUp/Catalog.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Renovators
+{
+    public class Catalog
+    {
+        public string Name { get; set; }
+        public int NeededRenovators { get; set; }
+        public string Project { get; set; }
+
+        public List<Renovator> Renovators { get; private set; }
+
+        public Catalog(string name, int neededRenovators, string project)
+        {
+            this.Name = name;
+            this.NeededRenovators = neededRenovators;
+            this.Project = project;
+            this.Renovators = new List<Renovator>();
+        }
+
+        public int Count => this.Renovators.Count;
+
+        public string AddRenovator(Renovator renovator)
+        {
+
+            if (renovator.Name == null || renovator.Name == " ")
+            {
+                return "Invalid renovator's information.";
+            }
+            if (this.NeededRenovators == this.Renovators.Count)
+            {
+                return "Renovators are no more needed.";
+            }
+
+            if (renovator.Rate > 350)
+            {
+                return "Invalid renovator's rate.";
+            }
+            this.Renovators.Add(renovator);
+            return $"Successfully added {renovator.Name} to the catalog.";
+        }
+
+        public bool RemoveRenovator(string name)
+        {
+            Renovator renovator = this.Renovators.FirstOrDefault(n => n.Name == name);
+            return this.Renovators.Remove(renovator);
+        }
+
+        public int RemoveRenovatorBySpecialty(string type)
+        {
+            int count = this.Renovators.RemoveAll(x => x.Type == type);
+            return count;
+
+        }
+
+        public Renovator HireRenovator(string name)
+        {
+            Renovator ren = this.Renovators.FirstOrDefault(x => x.Name == name);
+            if (ren == null)
+            {
+                return null;
+            }
+            ren.Hired = true;
+            return ren;
+        }
+
+        public List<Renovator> PayRenovators(int days)
+        {
+            List<Renovator> renovators = this.Renovators.Where(n => n.Days > days).ToList();
+            return renovators;
+
+        }
+
+        public string Report()
+        {
+            var notHired = this.Renovators.Where(x => x.Hired == false).ToList();
+            return $"Renovators available for Project {this.Project}: \n" +
+                $"{String.Join("\n", notHired)} ";
+        }
+    }
+}
---
+++ EXAM_PREP/Renovators/Renovators/StartUp/Renovator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Renovators
+{
+    public class Renovator
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Rate { get; set; }
+        public int Days { get; set; }
+        public bool Hired { get; set; } = false;
+
+        public Renovator(string name, string type, double rate, int days)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Rate = rate;
+            this.Days = days;
+        }
+
+        public override string ToString()
+        {
+            return $"-Renovator: {this.Name} \r\n" +
+                   $"--Specialty: {this.Type}\r\n" +
+                   $"--Rate per day: {this.Rate} BGN";
+        }
+    }
+}
