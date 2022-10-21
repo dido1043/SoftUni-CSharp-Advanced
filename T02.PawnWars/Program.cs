8e681398446628e19635f8a154e82e0f90591f46
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2022-10-21 14:36:48 +0300

    up

---
+++ T02.PawnWars/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace T02.PawnWars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int size = 8;
+
+            char[,] matrix = new char[size, size];
+
+            int blackR = 0;
+            int blackC = 0;
+            int whiteR = 0;
+            int whiteC = 0;
+            for (int row = 0; row < size; row++)
+            {
+                var input = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = input[col];
+                    if (matrix[row,col] == 'w')
+                    {
+                        whiteR = row;
+                        whiteC = col;
+                    }
+                    if (matrix[row, col] == 'b')
+                    {
+                        blackR = row;
+                        blackC = col;
+                    }
+                }
+            }
+            bool isWhite = true;
+
+            while (true)
+            {
+                if (isWhite)
+                {
+                    if (IsValid(matrix, whiteR - 1, whiteC - 1) && matrix[whiteR - 1, whiteC - 1] == 'b')
+                    {
+                        whiteR--;
+                        whiteC--;
+                        Console.WriteLine($"Game over! White capture on {(char)(whiteC + 97)}{8 - whiteR}.");
+                        return;
+                    }
+                    //if (IsValid(matrix, whiteR - 1, whiteC))
+                    //{ 
+                        whiteR--;
+                        matrix[whiteR, whiteC] = 'w';
+
+                        if (whiteR == 0)
+                        {
+                            Console.WriteLine($"Game over! White pawn is promoted to a queen at {(char) (whiteC + 97)}{matrix.GetLength(0)}.");
+                            return;
+                        }
+                   // }
+                }
+                else
+                {
+                    if (IsValid(matrix, blackR + 1, blackC - 1) && matrix[blackR + 1, blackC - 1] == 'w')
+                    {
+                        blackR++;
+                        blackC--;
+
+                        Console.WriteLine($"Game over! Black capture on {(char)(blackC + 97)}{8 - blackR}.");
+                        return;
+                    }
+                    //if (IsValid(matrix, blackR + 1, blackC))
+                    //{
+                        blackR++;
+                        matrix[blackR, blackC] = 'b';
+
+                        if (blackR == matrix.GetLength(0))
+                        {
+                            Console.WriteLine($"Game over! Black pawn is promoted to a queen at {(char)(blackC + 97)}{matrix.GetLength(0)}.");
+                            return;
+                        }
+                    //}
+                }
+                isWhite = !isWhite;
+            }
+        }
+
+        private static bool IsValid(char[,] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.GetLength(0) && col >= 0 && row < matrix.GetLength(1);
+        }
+    }
+}
---
+++ T02.TheBattleOfTheFiveArmies/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+namespace T02.TheBattleOfTheFiveArmies
+{
+
+  
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int armorOfArmy = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            char[][] matrix = new char[n][];
+
+            for (int row = 0; row < n; row++)
+            {
+                var input = Console.ReadLine().ToCharArray();
+                matrix[row] = input;
+            }
+
+            var position = FindArmy(matrix);
+            int currentRow = position[0];
+            int currentCol = position[1];
+
+            bool isWin = false;
+            bool isDefeat = false;
+            var commands = Console.ReadLine();
+            while (true)
+            {
+
+
+
+                armorOfArmy--;
+                var tokens = commands.Split(" ");
+                var command = tokens[0];
+                var orcRow = int.Parse(tokens[1]);
+                var orcCol = int.Parse(tokens[2]);
+                if (IsValid(matrix, orcRow, orcCol))
+                {
+                    matrix[orcRow][orcCol] = 'O';
+                }
+                switch (command)
+                {
+                    case "up":
+                        if (IsValid(matrix, currentRow - 1, currentCol))
+                        {
+                            matrix[currentRow][currentCol] = '-';
+                            currentRow--;
+                        }
+                        else
+                        {
+                            armorOfArmy--; 
+                        }
+                        break;
+                    case "down":
+                        if (IsValid(matrix, currentRow + 1, currentCol))
+                        {
+                            matrix[currentRow][currentCol] = '-';
+                            currentRow++;
+                        }
+                        else
+                        { 
+                            armorOfArmy--; 
+                        }
+                        break ;
+                    case "left":
+                        if (IsValid(matrix, currentRow, currentCol - 1))
+                        {
+                            matrix[currentRow][currentCol] = '-';
+                            currentCol--;
+                        }
+                        else
+                        { 
+                            armorOfArmy--; 
+                        }
+                        break;
+                    case "right":
+                        if (IsValid(matrix, currentRow, currentCol + 1))
+                        {
+                            matrix[currentRow][currentCol] = '-';
+                            currentCol++;
+                        }
+                        else
+                        {
+                            armorOfArmy--;
+                        }
+                        break;
+                }
+                if (matrix[currentRow][currentCol] == 'M')
+                {
+                    matrix[currentRow][currentCol] = '-';
+                    isWin = true;
+                    break;
+                    
+                }
+                if (matrix[currentRow][currentCol] == 'O')
+                {
+                    armorOfArmy -= 2;
+                }
+                if (armorOfArmy <= 0)
+                {
+                    matrix[currentRow][currentCol] = 'X';
+                    isDefeat = true;
+                    break;
+                }
+                commands = Console.ReadLine();
+            }
+            if(isWin)
+                Console.WriteLine($"The army managed to free the Middle World! Armor left: {armorOfArmy}");
+            else if(isDefeat)
+                Console.WriteLine($"The army was defeated at {currentRow};{currentCol}.");
+            PrintMatrix(matrix);
+        }
+
+        private static void PrintMatrix(char[][] matrix)
+        {
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                Console.WriteLine(String.Join("", matrix[r]));
+            }
+        }
+
+        private static int[] FindArmy(char[][] matrix)
+        {
+            int[] result = new int[2];
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    if (matrix[row][col] == 'A')
+                    {
+                        result[0] = row;
+                        result[1] = col;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static bool IsValid(char[][] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.Length && col >= 0 && row < matrix[row].Length;
+        }
+    }
+}
+
